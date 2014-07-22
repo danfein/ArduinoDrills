2e6e31468bd3f34cf1e8a09ce79e61af6387b051
Dan Fein <dpfein@gmail.com>
2014-07-21 23:38:01 -0700

    Add delay to button state check
    Add delay to button state check
    Clean up  var declaration

--- sketch_jul21b_Ex03b/sketch_jul21b_Ex03b.ino
+++ sketch_jul21b_Ex03b/sketch_jul21b_Ex03b.ino
@@ -1,10 +1,10 @@
-// Example 03b: Turn on the LED when the button is pressed and keep it on after relased
+// Example 03c: Turn on the LED when the button is pressed and keep it on after relased
 
-const int LED = 13; // the pin for the LED
-const int button = 7; //the input pin for the button
-int val = 0; //val will store the state of button
-int old_val = 0; //Store the previous value of val
-int state = 0; //0 = LED off - 1= LED on
+const int LED = 13;        // the pin for the LED
+const int button = 7;     // the input pin for the button
+int val = 0;             // val will store the state of button
+int old_val = 0;        // Store the previous value of val
+int state = 0;         // 0 = LED off, 1= LED on
 
 void setup(){
   pinMode(LED, OUTPUT); //set pin 13 to output for the LED
@@ -17,6 +17,7 @@ val = digitalRead(button); //read input value from button, set to val
 // check if there was a transition
 if ((val == HIGH) && (old_val == LOW)){
   state = 1 - state;
+  delay(100); // 100ms delay to ignore pushbutton noise
 }
 
 old_val = val; // val is now old, let's store it
