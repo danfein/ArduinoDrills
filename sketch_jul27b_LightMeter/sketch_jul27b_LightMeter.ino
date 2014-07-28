b0df8373db759f84f9f6b5ab25d0bd364911121b
Dan Fein <dpfein@gmail.com>
2014-07-28 00:51:17 -0700

    Function to replace repeated code
    I learned what functions are, because repeating code over and over is
    gross.

--- sketch_jul27b_LightMeter/sketch_jul27b_LightMeter.ino
+++ sketch_jul27b_LightMeter/sketch_jul27b_LightMeter.ino
@@ -1,16 +1,18 @@
-// basic light meter
+// basic light meter, now with functions
 
 int val = 0; //variable for value coming from light sensor
-int d = 150; //Delay variable
-void blinkR( int x ) //function to blink
-  {
-    digitalWrite(x, HIGH);
-    /*
-    delay(d);
-    digitalWrite(x, LOW);
-    delay(d);
-    */
-  }
+
+void trigger(int V, int P) //V is trigger threshold, p is pin to trigger
+{
+   if (val > V)
+    {
+      digitalWrite(P, HIGH);    
+    }
+    else
+    {
+      digitalWrite(P, LOW); 
+    }   
+}
 
 void setup()
 {
@@ -27,48 +29,10 @@ void loop()
 {
   val = analogRead(0); //read value from analog pin zero
   //Serial.println(val); //print sensor value to serial for diagnostic
-  if (val > 50)
-    {
-      blinkR(3); //call blinkR function, send it pin number
-    }
-    else
-    {
-      digitalWrite(3, LOW); //turn LED off
-    }  
-  
-  if (val > 75)
-    {
-      blinkR(5); //turn LED on
-    }
-    else
-    {
-      digitalWrite(5, LOW); //turn LED off
-    }  
-
-  if (val > 100)
-    {
-      blinkR(6); //turn LED on
-    }
-    else
-    {
-      digitalWrite(6, LOW); //turn LED off
-    }  
-
-  if (val > 200)
-    {
-      blinkR(9); //turn LED on
-    }
-    else
-    {
-      digitalWrite(9, LOW); //turn LED off
-    }  
-
-  if (val > 300)
-    {
-      blinkR(10); //turn LED on
-    }
-    else
-    {
-      digitalWrite(10, LOW); //turn LED off
-    }  
+  trigger(50,3);
+  trigger(75,5);
+  trigger(100,6);
+  trigger(150,9);
+  trigger(200,10); 
+ 
 }
